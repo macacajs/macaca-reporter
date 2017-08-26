5f88d6f2b049afc646bce0ccca589dc4508be34d
xudafeng <xudafeng@126.com>
2017-08-26 17:42:28 +0800

    add mock

---
+++ test/mock.js
@@ -0,0 +1,55 @@
+module.exports = () => {
+  return {
+    stats: {
+      suites: 0,
+      tests: 0,
+      passes: 3,
+      pending: 0,
+      failures: 0,
+      start: new Date(),
+      passPercent: 50,
+      pendingPercent: 0,
+      other: 0,
+      hasOther: false,
+      skipped: 1,
+      hasSkipped: true,
+      duration: 14523,
+      end: new Date()
+    },
+    suites: {
+      title: '',
+      ctx: {},
+      suites: [],
+      tests: [],
+      pending: [],
+      _beforeEach: [],
+      _beforeAll: [],
+      _afterEach: [],
+      _afterAll: [],
+      root: true,
+      _timeout: 2000,
+      _enableTimeouts: true,
+      _slow: 75,
+      _retries: -1,
+      delayed: false,
+      _events: {
+        'pre-require': []
+      },
+      _eventsCount: 1,
+      uuid: 'xxxxxxxxxxx',
+      fullFile: '',
+      file: '',
+      passes: [],
+      failures: [],
+      skipped: [],
+      totalTests: 0,
+      totalPasses: 0,
+      totalFailures: 0,
+      totalPending: 0,
+      totalSkipped: 0,
+      duration: 0,
+      _totalTime: 14523
+    }
+  };
+};
+
