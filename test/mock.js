db598c2bb6e6520cef6668ecc2e37084fb9ec904
xudafeng <xudafeng@126.com>
2017-11-05 11:16:46 +0800

    tweak format of test suites

--- test/mock.js
+++ test/mock.js
@@ -1,3 +1,5 @@
+const suites = require('./suites');
+
 module.exports = {
   stats: {
     suites: 0,
@@ -15,40 +17,7 @@ module.exports = {
     duration: 14523,
     end: new Date()
   },
-  suites: {
-    title: '',
-    ctx: {},
-    suites: [],
-    tests: [],
-    pending: [],
-    _beforeEach: [],
-    _beforeAll: [],
-    _afterEach: [],
-    _afterAll: [],
-    root: true,
-    _timeout: 2000,
-    _enableTimeouts: true,
-    _slow: 75,
-    _retries: -1,
-    delayed: false,
-    _events: {
-      'pre-require': []
-    },
-    _eventsCount: 1,
-    uuid: 'xxxxxxxxxxx',
-    fullFile: '',
-    file: '',
-    passes: [],
-    failures: [],
-    skipped: [],
-    totalTests: 0,
-    totalPasses: 0,
-    totalFailures: 0,
-    totalPending: 0,
-    totalSkipped: 0,
-    duration: 0,
-    _totalTime: 14523
-  },
+  suites: suites || {},
   current: {
     image: 'https://macacajs.github.io/macaca-logo/svg/monkey.svg',
     list: [{
