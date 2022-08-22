a2c9487bb012510c098941073ac113b6b5263a8a
达峰的夏天 <xudafeng@126.com>
2022-08-22 21:12:12 +0800

    refactor: suites redesign (#8)
    * refactor: suites redesign

--- assets/components/Mind.js
+++ assets/components/Mind.js
@@ -1,55 +1,46 @@
-import React from 'react';
-import remove from 'lodash/remove';
+import React, { useEffect } from 'react';
 import Editor from '@antv/g6-editor';
 import { guid } from '@/common/helper';
+import styles from './Mind.module.less';
 
-import './Mind.less';
+function _transformTree(suites = []) {
+  suites.forEach((suite) => {
+    suite.name = suite.title;
+    suite.children = suite.tests;
+    suite.id = guid();
+    if (suite?.suites?.length) {
+      suite.children = suite.children.concat(suite.suites);
+    }
+    if (suite.children) {
+      _transformTree(suite.children);
+    }
+  });
+  return suites;
+}
 
-export default class Mind extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
+const adaptor = (listData, isRoot) => {
+  const midNum = parseInt(listData.length / 2, 10);
+  return listData.map((item, index) => {
+    item.label = item.title;
+    if (isRoot) {
+      item.side = index > midNum ? 'left' : 'right';
+    }
+    if (item?.children?.length) {
+      item.children = adaptor(item.children);
+    }
+    return item;
+  });
+};
 
-  _transformTree(suites = []) {
-    suites.forEach((suite) => {
-      suite.name = suite.title;
-      suite.children = suite.tests;
-      suite.id = guid();
-      if (suite?.suites?.length) {
-        suite.children = suite.children.concat(suite.suites);
-      }
-      if (suite.children) {
-        this._transformTree(suite.children);
-      }
-    });
-    return suites;
-  }
-
-  componentDidMount() {
-    const { title } = this.props;
-    let { suites } = this.props;
-    suites = this._transformTree(suites);
-
-    const adaptor = (listData, isRoot) => {
-      const midNum = parseInt(listData.length / 2, 10);
-      return listData.map((item, index) => {
-        item.label = item.title;
-        if (isRoot) {
-          item.side = index > midNum ? 'left' : 'right';
-        }
-        if (item?.children?.length) {
-          item.children = adaptor(item.children);
-        }
-        return item;
-      });
-    };
+export default function Mind(props) {
+  useEffect(() => {
+    const { title, suites } = props;
+    const _suites = _transformTree(suites);
 
     const mindSuite = {
       roots: [{
         label: title,
-        children: adaptor(suites, true),
+        children: adaptor(_suites, true),
       }],
     };
 
@@ -64,11 +55,9 @@ export default class Mind extends React.Component {
         width: window.innerWidth,
       },
     });
-  }
+  }, []);
 
-  render() {
-    return (
-      <div id="mind-node" className="mind-node"></div>
-    );
-  }
+  return (
+    <div id="mind-node" className={styles.wrapper} />
+  );
 }
--- assets/components/Screen.js
+++ assets/components/Screen.js
@@ -5,45 +5,39 @@ import {
 } from 'antd';
 import './Screen.less';
 
-export default class NavBar extends React.Component {
-  constructor(props) {
-    super(props);
+export default function NavBar(props) {
+  const { current } = props;
+
+  if (!current) {
+    return null;
   }
 
-  render() {
-    const { current } = this.props;
-
-    if (!current) {
-      return null;
-    }
-
-    return (
-      <div className="screen">
-        <Row
-          gutter={24}
-          type="flex"
-          justify="space-around"
-        >
-          <Col span={16}>
-            <div className="imagecon">
-              <h1>Current Screen</h1>
-              <img src={current.image} />
-            </div>
-          </Col>
-          <Col span={6}>
-            <div>
-              <h1>Current status</h1>
-              {
-                current.list.map((item, key) => {
-                  return (
-                    <p key={key}>{ item.title }: { item.value }</p>
-                  );
-                })
-              }
-            </div>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
+  return (
+    <div className="screen">
+      <Row
+        gutter={24}
+        type="flex"
+        justify="space-around"
+      >
+        <Col span={16}>
+          <div className="imagecon">
+            <h1>Current Screen</h1>
+            <img src={current.image} />
+          </div>
+        </Col>
+        <Col span={6}>
+          <div>
+            <h1>Current status</h1>
+            {
+              current.list.map((item, key) => {
+                return (
+                  <p key={key}>{ item.title }: { item.value }</p>
+                );
+              })
+            }
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
 }
