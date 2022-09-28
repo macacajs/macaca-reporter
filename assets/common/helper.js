18bbb2b5b84ebe86715246d4cf81bd0e60a51994
barry <100205797+barry3406@users.noreply.github.com>
2022-09-28 15:51:00 +0800

    feat: tweak time display format (#14)

--- assets/common/helper.js
+++ assets/common/helper.js
@@ -34,3 +34,33 @@ export const validVideo = (file) => {
 export const validImage = (file) => {
   return file.includes('.png') || file.includes('.jpg') || file.includes('.gif');
 };
+
+export const formatSeconds = (value) => {
+  if (!value || typeof value !== 'number' || value < 1) {
+    console.warn('can not format seconds', value);
+    return '0s';
+  }
+  const secondTime = Math.ceil(value / 1000);
+
+  let seconds = secondTime;
+  let minutes = 0;
+  let hour = 0;
+
+  if (seconds > 60) {
+    minutes = parseInt(seconds / 60, 10);
+    seconds = parseInt(seconds % 60, 10);
+    if (minutes > 60) {
+      hour = parseInt(minutes / 60, 10);
+      minutes = parseInt(minutes % 60, 10);
+    }
+  }
+
+  let finalTime = `${seconds}s`;
+  if (minutes > 0) {
+    finalTime = `${minutes}m ${finalTime}`;
+    if (hour > 0) {
+      finalTime = `${hour}h ${finalTime}`;
+    }
+  }
+  return finalTime;
+};
