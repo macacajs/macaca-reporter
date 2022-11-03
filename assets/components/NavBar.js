4bed0d000bf565fb8fae946a20d3439067313bef
yihuineng <471110230@qq.com>
2022-11-03 16:46:23 +0800

    feat: support display env info

--- assets/components/NavBar.js
+++ assets/components/NavBar.js
@@ -10,11 +10,16 @@ import styles from './NavBar.module.less';
 
 export default function NavBar(props) {
   const { stats } = props;
+  const { envInfo = {} } = stats;
+  let envInfoStr = `reporter_version: ${pkg.version}`;
+  for (const key in envInfo) {
+    envInfoStr += `\r\n${key}: ${envInfo[key]}`;
+  }
   return (
     <ul className="head">
       <li>
         <a href={pkg.homepage}>
-          <div title={`version: ${pkg.version}`} className={styles.info}>
+          <div title={envInfoStr} className={styles.info}>
             <Icon
               width={60}
               type="monkey"
@@ -27,38 +32,40 @@ export default function NavBar(props) {
       </li>
       <li>
         <h5>suites</h5>
-        <p>{ stats.suites }</p>
+        <p>{stats.suites}</p>
       </li>
       <li>
         <h5>tests</h5>
-        <p>{ stats.tests }</p>
+        <p>{stats.tests}</p>
       </li>
       <li>
         <h5>passes</h5>
-        <p style={{ color: '#a5d86e' }}>{ stats.passes }</p>
+        <p style={{ color: '#a5d86e' }}>{stats.passes}</p>
       </li>
       <li>
         <h5>failures</h5>
-        <p style={{ color: '#df5869' }}>{ stats.failures }</p>
+        <p style={{ color: '#df5869' }}>{stats.failures}</p>
       </li>
       <li>
         <h5>pending</h5>
-        <p style={{ color: 'rgb(234, 187, 56)' }}>{ stats.pending }</p>
+        <p style={{ color: 'rgb(234, 187, 56)' }}>{stats.pending}</p>
       </li>
       <li>
         <h5>skipped</h5>
-        <p style={{ color: '#898989' }}>{ stats.skipped }</p>
+        <p style={{ color: '#898989' }}>{stats.skipped}</p>
       </li>
       <li>
         <h5>duration</h5>
-        <p>{ formatSeconds(stats.duration) }</p>
+        <p>{formatSeconds(stats.duration)}</p>
       </li>
       <li>
         <div className="head-circle">
           <Progress
             type="circle"
             percent={stats.passPercent}
-            format={percent => { return `${percent}%`; }}
+            format={percent => {
+              return `${percent}%`;
+            }}
             status={stats.passPercent >= 90 ? 'success' : 'exception'}
             width={50}
           />
